6dc338e51b0582576b7a671eb303f7fd7db677a7
Yoshie <yoshiemuranaka@gmail.com>
2014-11-10 09:58:27 -0500

    trying to add database

--- db/connection.rb
+++ db/connection.rb
@@ -1,10 +1,12 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "yoshiemuranaka",
-  :database => "contact_list"
-})
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/contact_list')
+
+# ActiveRecord::Base.establish_connection({
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "yoshiemuranaka",
+#   :database => "contact_list"
+# })
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
